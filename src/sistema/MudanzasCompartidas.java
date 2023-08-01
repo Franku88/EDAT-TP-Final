4573720b0349a515d52048d26c6eaaccc087dc56
franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2023-08-01 03:05:55 -0300

    01/08/2023

---
+++ src/sistema/MudanzasCompartidas.java
@@ -0,0 +1,26 @@
+package sistema;
+public class MudanzasCompartidas {
+    
+    public static void main(String[] args) {
+        
+        opciones();
+    }
+
+    public static void opciones() {
+        System.out.println("---------------- Menu ----------------");
+        System.out.println("    1. Carga inicial del sistema");
+        System.out.println("*------------------------------------*");
+        System.out.println("    2. ABM Ciudades");
+        System.out.println("    3. ABM Red de rutas");
+        System.out.println("    4. ABM Clientes");
+        System.out.println("    5. ABM Pedidos");
+        System.out.println("*------------------------------------*");
+        System.out.println("    6. Consulta Cliente");
+        System.out.println("    7. Consulta Ciudades");
+        System.out.println("    8. Consulta Viajes");
+        System.out.println("    9. Verificar Viaje");
+        System.out.println("--------------------------------------");
+        System.out.println("    10. Mostrar Sistema");
+        System.out.println("--------------------------------------");
+    }
+}
